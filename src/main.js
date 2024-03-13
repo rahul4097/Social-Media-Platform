019b1da252c2b1a153772e37d66dea4eeb154517
rahul4097 <rk308238@gmail.com>
2024-03-13 19:25:44 +0530

    changed

--- src/main.js
+++ src/main.js
@@ -1,40 +1,44 @@
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const methodOverride = require('method-override')
+// const bodyParser = require('body-parser');
+// const session = require('express-session')
+// const {v4:uuidv4} = require("uuid")
 require('./database/db');
 const path = require('path')
 const ejs = require('ejs')
 
 const userAuthRouter = require('../routes/authenticationRoute')
 const profileRouter = require('../routes/profileRoute')
-const postRouter = require('../routes/postRoute')
-const userModel = require('../models/userLogin')
+const postRouter = require('../routes/postRoute');
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
+// app.use(bodyParser.json)
+app.use(methodOverride('_method'))
+// app.use(bodyParser.urlencoded({extended : true}))
+
 app.set('view engine', "ejs")
 app.use(express.urlencoded({extended : true}))
 
-const body = path.join(__dirname,"../templates/views")
+app.use(expressLayouts);
+app.set('layout','./layouts/main');
+
+
+const body = path.join(__dirname,"../templates")
+// app.use(session({
+//     secret: uuidv4(),
+//     resave: false,
+//     saveUninitialized:true
+// }))
 
 app.set('views',body)
 
-app.get('/templates/views/homePage.ejs',(req , res)=>{
-    res.render('homePage')
-})
-app.get('/templates/views/signup.ejs',(req , res)=>{
-    res.render('signup')
-})
-app.get('/templates/views/login.ejs',(req , res)=>{
-    res.render('login')
-})
-app.get('/templates/views/profile.ejs',(req , res)=>{
-    res.render('profile')
-})
 
-
-app.use('/users', userAuthRouter)
-app.use('/users', profileRouter)
-app.use('/users', postRouter)
+app.use('/', userAuthRouter)
+app.use('/profile', profileRouter)
+app.use('/post', postRouter)
 
 app.listen(port , ()=>{
     console.log(`The server has been connected on port ${port}`)
